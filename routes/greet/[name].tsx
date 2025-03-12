b6b17afe59986ee3a9674c103d01ca82dc630b6c
sobebarali <sobebar_ali17@hotmail.com>
2025-03-12 16:21:03 +0530

    Enhance greet route with improved styling and formatting

--- routes/greet/[name].tsx
+++ routes/greet/[name].tsx
@@ -1,5 +1,10 @@
 import { PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params;
+  return (
+    <main>
+      <p>Greetings to you, {name}!</p>
+    </main>
+  );
 }
